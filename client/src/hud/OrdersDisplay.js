5e0e9d0c7897ae4c6aaf671aa2617f8cf2942036
Richard To <richard@hazelanalytics.com>
2021-05-30 16:46:49 -0700

    Update HUD style

--- client/src/hud/OrdersDisplay.js
+++ client/src/hud/OrdersDisplay.js
@@ -8,16 +8,17 @@ class OrdersDisplay extends Phaser.GameObjects.Container {
 
     this.orders = orders
 
-    this.horizontalSpace = 200
-    this.topPadding = 80
-    this.leftPadding = 40
-    this.fontSize = 50
+    this.horizontalSpace = 150
+    this.leftPadding = 20
+    this.topPadding = 20
+    this.fontSize = 36
     this.textColor = '#FFFFFF'
+    this.backgroundColor = '#ECECEC'
 
     scene.add.existing(this)
 
     const leftTopCorner = Settings.LEFT_TOP_CORNER
-    this.setPosition(leftTopCorner.x + this.topPadding, leftTopCorner.y + this.leftPadding)
+    this.setPosition(leftTopCorner.x + this.leftPadding, leftTopCorner.y + this.topPadding)
     this.setScrollFactor(0)
     this.setDepth(2)
     this._setupOrders()
@@ -38,17 +39,34 @@ class OrdersDisplay extends Phaser.GameObjects.Container {
   _createOrdersBoard() {
     const orderContainers = []
 
+    // Add HUD box
+    const rect = this.scene.add.graphics()
+    rect.fillStyle(0x222, 0.5)
+    rect.fillRoundedRect(0, 0, 430, 175, 5)
+    rect.strokeRoundedRect(0, 0, 430, 175, 5)
+    rect.lineStyle(2, 0x222, 1)
+    orderContainers.push(rect)
+
     // Add Orders Text
-    const scoreText = this.scene.add.text(0, -20, 'Orders:',{fontSize: `${this.fontSize}px`, fill: this.textColor })
+    const scoreText = this.scene.add.text(
+      15,
+      15,
+      'orders:',
+      {
+        fill: this.textColor,
+        fontFamily: 'Arial',
+        fontSize: `${this.fontSize}px`,
+      },
+    )
     orderContainers.push(scoreText)
 
     // Add orders with Burger sprites for every order
-    let horizontalSpace = 0
+    let horizontalSpace = 15
     this.orders.forEach(order => {
       orderContainers.push(
-        this.scene.add.image(horizontalSpace, 180, 'assets', order.frame)
+        this.scene.add.image(horizontalSpace, 150, 'assets', order.frame)
         .setOrigin(0, 1)
-        .setScale(1.2)
+        .setScale(0.8)
       )
       horizontalSpace += this.horizontalSpace
     })
